6f7e7411f65e61812b9e289016b74053d2cfa26e
Ktrygub <konstantin.trygub@gmail.com>
2018-01-25 00:59:49 +0200

    lesson0 task

--- src/lesson0/task0.js
+++ src/lesson0/task0.js
@@ -1,5 +1,21 @@
-export function sum(a, b) {
-  return a + b;
+// Adding any amount of arguments
+// Throws an Error if any of the arguments is Oject, Array or Boolean type
+
+export function sum(...rest) {
+  const args = [...rest];
+  const invalidTypes = ['object', 'boolean'];
+
+  if (args.length === 0) {
+    return 0;
+  }
+
+  args.forEach(el => {
+    if (invalidTypes.includes(typeof el)) {
+      throw Error('Invalid parameters');
+    }
+  });
+
+  return args.reduce((previousValue, currentValue) => previousValue + currentValue);
 }
 
 export default {
