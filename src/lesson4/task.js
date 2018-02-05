bc586fcf1f54d0a960bbc0a79f288ac7ec0f8515
Ktrygub <konstantin.trygub@gmail.com>
2018-02-06 01:02:52 +0200

    L4 draft

--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -8,8 +8,53 @@
   in forEach() method.
   mySet = createSet(['a', 'b', 'c'])
 */
-export function createSet(arr) {
 
+// TODO: make CustomSet and CustomMap iterable
+// TODO: class refactoring
+
+function CustomSet(arr) {
+  this.entries = [];
+  arr.forEach(el => this.add(el));
+}
+
+CustomSet.prototype = {
+  get size() {
+    return this.entries.length;
+  },
+
+  add(value) {
+    if (!this.has(value)) {
+      this.entries.push(value);
+    }
+    return this;
+  },
+
+  delete(value) {
+    if (this.has(value)) {
+      this.entries = this.entries.filter(el => !((Number.isNaN(value) && Number.isNaN(el)) || el === value));
+      return true;
+    }
+    return false;
+  },
+
+  has(value) {
+    return this.entries.includes(value);
+  },
+
+  forEach(callback, thisArg = this) {
+    thisArg.entries.forEach((el, index, array) => {
+      const value = el;
+      callback(value, value, array);
+    });
+  },
+
+  clear() {
+    this.entries = [];
+  },
+};
+
+export function createSet(arr = []) {
+  return new CustomSet(arr);
 }
 
 /*
@@ -22,11 +67,60 @@ export function createSet(arr) {
   in forEach() method.
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
-export function createMap(arr) {
 
+function CustomMap(arr) {
+  this.entries = [];
+  arr.forEach(el => this.set(...el));
+}
+
+CustomMap.prototype = {
+  get size() {
+    return this.entries.length;
+  },
+
+  get(key) {
+    return this.entries.find(el => el[0] === key || (Number.isNaN(key) && Number.isNaN(el[0])));
+  },
+
+  has(key) {
+    return !!this.get(key);
+  },
+
+  set(key, value) {
+    if (!this.has(key)) {
+      this.entries.push([key, value]);
+    } else {
+      const updateElement = this.get(key);
+      updateElement[1] = value;
+    }
+    return this;
+  },
+
+  delete(key) {
+    if (this.has(key)) {
+      this.entries = this.entries.filter(el => !((Number.isNaN(key) && Number.isNaN(el[0])) || el[0] === key));
+      return true;
+    }
+    return false;
+  },
+
+  forEach(callback, thisArg = this) {
+    thisArg.entries.forEach((el, index, array) => {
+      const [key, value] = el;
+      callback(value, key, array);
+    });
+  },
+
+  clear() {
+    this.entries = [];
+  },
+};
+
+export function createMap(arr = []) {
+  return new CustomMap(arr);
 }
 
 export default {
   createSet,
-  createMap
+  createMap,
 };
