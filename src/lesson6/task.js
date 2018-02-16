c98a89c8bc904ea929745c895216dce2bc026001
Ktrygub <konstantin.trygub@gmail.com>
2018-02-16 15:17:10 +0200

    L6 with async/await

--- src/lesson6/task.js
+++ src/lesson6/task.js
@@ -8,64 +8,46 @@ function findExactMatchName(collection, name) {
 
 // Климат на любой планете по её имени
 // {planetName} – String
-const getClimate = function getClimate(planetName) {
-  return axios
-    .get(`https://swapi.co/api/planets/?search=${planetName}`)
-    .then(res => {
-      // res.data.results contains array with planets partially matched with {planetName},
-      // f.e. getClimate('tooine') will match with 'Dantooine' and 'Tatooine' planets.
-      const planets = res.data.results;
-      const planet = findExactMatchName(planets, planetName);
-      if (!planet) {
-        throw new Error('There is no such planet in Star Wars Universe.');
-      }
-      return planet.climate;
-    })
-    .catch(error => {
-      throw error;
-    });
+const getClimate = async function getClimate(planetName) {
+  const res = await axios.get(`https://swapi.co/api/planets/?search=${planetName}`);
+  const planets = res.data.results;
+  const planet = findExactMatchName(planets, planetName);
+  if (!planet) {
+    throw new Error('There is no such planet in Star Wars Universe.');
+  }
+  return planet.climate;
 };
 
 // Получить информацию (Object) о любом персонаже по имени
 // {name} – String
-const getProfile = function getProfile(name) {
-  return axios
-    .get(`https://swapi.co/api/people/?search=${name}`)
-    .then(res => {
-      const people = res.data.results;
-      const person = findExactMatchName(people, name);
-      if (!person) {
-        throw new Error('There is no such person in Star Wars Universe.');
-      }
-      return person;
-    })
-    .catch(error => {
-      throw error;
-    });
+const getProfile = async function getProfile(name) {
+  const res = await axios.get(`https://swapi.co/api/people/?search=${name}`);
+  const people = res.data.results;
+  const person = findExactMatchName(people, name);
+  if (!person) {
+    throw new Error('There is no such person in Star Wars Universe.');
+  }
+  return person;
 };
 
 // Получить список пилотов (имена в виде Array of Strings) космического корабля
 // по его названию
 // {starshipName} - String
 
-const getPilots = function getPilots(starshipName) {
-  return axios
-    .get(`https://swapi.co/api/starships/?search=${starshipName}`)
-    .then(res => {
-      const starships = res.data.results;
-      const starship = findExactMatchName(starships, starshipName);
-      if (!starship) {
-        throw new Error('There is no such starship in Star Wars Universe.');
-      }
-      return starship.pilots;
-    })
-    .then(pilotsUrl => {
-      const getPilotsNames = pilotsUrl.map(url => axios.get(url).then(res => res.data.name));
-      return Promise.all(getPilotsNames);
-    })
-    .catch(error => {
-      throw error;
-    });
+const getPilots = async function getPilots(starshipName) {
+  const res = await axios.get(`https://swapi.co/api/starships/?search=${starshipName}`);
+  const starships = res.data.results;
+  const starship = findExactMatchName(starships, starshipName);
+  if (!starship) {
+    throw new Error('There is no such starship in Star Wars Universe.');
+  }
+  const pilotsUrl = starship.pilots;
+  const getPilotsNames = pilotsUrl.map(async pilotUrl => {
+    const pilot = await axios.get(pilotUrl);
+    const { name } = pilot.data;
+    return name;
+  });
+  return Promise.all(getPilotsNames);
 };
 
 export default {
